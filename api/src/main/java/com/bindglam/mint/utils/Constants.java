3e3d8491c569f50175acfeb7cce91a012d3a6c80
bindglam <woobeen0516@gmail.com>
2026-02-08 16:26:48 +0900

    refactor: Constants

--- api/src/main/java/com/bindglam/mint/utils/Constants.java
+++ api/src/main/java/com/bindglam/mint/utils/Constants.java
@@ -1,6 +1,9 @@
 package com.bindglam.mint.utils;
 
-public class Constants {
+public final class Constants {
     public static final String PLUGIN_ID = "mint";
     public static final String PLUGIN_NAME = "Mint";
+
+    private Constants() {
+    }
 }
