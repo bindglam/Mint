542c54f40010f204c16a48b24110dcc31733ad49
bindglam <woobeen0516@gmail.com>
2026-02-12 13:09:38 +0900

    chore: modrinth publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("standard-conventions")
     alias(libs.plugins.runTask.paper)
     alias(libs.plugins.shadow)
+    alias(libs.plugins.minotaur)
 }
 
 dependencies {
@@ -13,6 +14,9 @@ dependencies {
 val groupString = group.toString()
 val versionString = version.toString()
 val mcVersionString = property("minecraft_version").toString()
+val supportedVersions = listOf(
+    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11")
 
 val runServerAction = Action<RunServer> {
     version(mcVersionString)
@@ -51,5 +55,24 @@ tasks {
         }
         prefix("kotlin")
         prefix("org.bstats")
+    }
+}
+
+modrinth {
+    token = System.getenv("MODRINTH_API_TOKEN")
+    projectId = "mint-economy"
+    syncBodyFrom = rootProject.file("README.md").readText()
+    versionType = "release"
+    changelog = rootProject.file("changelogs/$versionString.md").readText()
+    versionNumber = versionString
+    versionName = versionString
+    modrinth {
+        uploadFile.set(tasks.shadowJar)
+        gameVersions = supportedVersions
+        dependencies {
+            optional.project(
+                "vaultunlocked"
+            )
+        }
     }
 }
