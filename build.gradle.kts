709fd79530c8a2d7887584ac09d6581e742429f1
bindglam <woobeen0516@gmail.com>
2026-02-12 13:14:05 +0900

    style: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ val versionString = version.toString()
 val mcVersionString = property("minecraft_version").toString()
 val supportedVersions = listOf(
     "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
-    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11")
+    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11"
+)
 
 val runServerAction = Action<RunServer> {
     version(mcVersionString)
