bf6bbb191ade592bda263ecfd9625b0ba608ca21
bindglam <woobeen0516@gmail.com>
2026-02-12 09:21:37 +0900

    fix: AccountManagerImpl

--- core/src/main/kotlin/com/bindglam/mint/manager/AccountManagerImpl.kt
+++ core/src/main/kotlin/com/bindglam/mint/manager/AccountManagerImpl.kt
@@ -1,6 +1,5 @@
 package com.bindglam.mint.manager
 
-import com.bindglam.mint.Mint
 import com.bindglam.mint.account.Account
 import com.bindglam.mint.account.AccountImpl
 import com.bindglam.mint.account.CachedAccount
@@ -8,7 +7,6 @@ import com.bindglam.mint.account.CachedAccountImpl
 import com.bindglam.mint.account.log.TransactionLoggerImpl
 import com.bindglam.mint.utils.Constants
 import java.util.*
-import java.util.concurrent.CompletableFuture
 
 object AccountManagerImpl : AccountManager {
     const val ACCOUNTS_TABLE_NAME = "${Constants.PLUGIN_ID}_accounts"
@@ -36,6 +34,6 @@ object AccountManagerImpl : AccountManager {
         cachedAccounts.remove(uuid)
     }
 
-    override fun getAccount(uuid: UUID): Account = AccountImpl(uuid)
+    override fun getAccount(uuid: UUID): Account = getCachedAccount(uuid) ?: AccountImpl(uuid)
     override fun getCachedAccount(uuid: UUID): CachedAccount? = cachedAccounts[uuid]
 }
