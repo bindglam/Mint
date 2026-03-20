115ab832790c864b6e36be7a88555fa3b04d1c45
bindglam <woobeen0516@gmail.com>
2026-03-21 05:31:38 +0900

    fix: redis sync scheduler

--- core/src/main/kotlin/com/bindglam/mint/manager/AccountManagerImpl.kt
+++ core/src/main/kotlin/com/bindglam/mint/manager/AccountManagerImpl.kt
@@ -5,12 +5,16 @@ import com.bindglam.mint.account.AccountImpl
 import com.bindglam.mint.account.log.TransactionLoggerImpl
 import com.bindglam.mint.utils.PLUGIN_ID
 import com.bindglam.mint.utils.plugin
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask
+import org.bukkit.Bukkit
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 object AccountManagerImpl : AccountManager, Managerial {
     const val ACCOUNTS_TABLE_NAME = "${PLUGIN_ID}_accounts"
     const val LOGS_TABLE_NAME = "${PLUGIN_ID}_logs"
+
+    private var redisSyncTask: ScheduledTask? = null
 
     override fun priority() = Managerial.Priority.of(-1, 1)
 
@@ -21,13 +25,14 @@ object AccountManagerImpl : AccountManager, Managerial {
         }
 
         if(context.config().database.redis.enabled.value() && context.config().database.redis.syncInterval.value() > 0) {
-            context.plugin().plugin().server.asyncScheduler.runAtFixedRate(context.plugin().plugin(), { _ ->
-                syncAllRedis()
-            }, 0L, context.config().database.redis.syncInterval.value().toLong(), TimeUnit.SECONDS)
+            redisSyncTask = Bukkit.getAsyncScheduler().runAtFixedRate(context.plugin().plugin(), { _ -> syncAllRedis() },
+                0L, context.config().database.redis.syncInterval.value().toLong(), TimeUnit.SECONDS)
         }
     }
 
     override fun end(context: Context) {
+        redisSyncTask?.cancel()
+
         syncAllRedis()
     }
 
