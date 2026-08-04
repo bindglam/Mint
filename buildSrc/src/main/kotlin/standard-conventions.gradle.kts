35fe8bddb73fcd6ac843d39bcd734951d62a9e33
bindglam <woobeen0516@gmail.com>
2026-08-04 23:47:19 +0900

    chore: update java to 25

--- buildSrc/src/main/kotlin/standard-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/standard-conventions.gradle.kts
@@ -27,9 +27,9 @@ tasks {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
